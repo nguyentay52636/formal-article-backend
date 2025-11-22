aeea53c271fa6176e6716cd4e16ffd67c05883aa
tay52636 <phuongtay52636@gmail.com>
2025-11-22 15:46:28 +0700

    feat: Add admin chat history management, template CRUD operations, detailed chat room view, and rating DTOs.

---
+++ src/main/java/org/example/dto/response/chat/ChatRoomDetailResponse.java
@@ -0,0 +1,18 @@
+package org.example.dto.response.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ChatRoomDetailResponse extends ChatRoomResponse {
+    private List<ChatMessageResponse> messages;
+}
